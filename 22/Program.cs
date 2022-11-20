a0c3c83798a0adadbfccde4fa8e4cfbbf2457dc7
Margarita <margoshka_d@mail.ru>
2022-11-20 22:58:07 +0300

    Homework 02

--- 22/Program.cs
+++ 22/Program.cs
@@ -1,5 +1,4 @@
 ﻿//22.Программа проверяет пятизначное число на палиндромом.
 
 int a=Convert.ToInt32(Console.ReadLine());
-int b=Convert.ToInt32(Console.ReadLine());
-if(a==b) System.Console.WriteLine($"{a} является палиндромом {b}"); else System.Console.WriteLine("NO");
+if(a%10==a/10000 && a/10%10==a/1000%10) System.Console.WriteLine($"{a} является палиндромом"); else System.Console.WriteLine("NO");
