7f7990c350c0577a5c1b847fce4242b38c99b128
Margarita <margoshka_d@mail.ru>
2022-11-27 18:09:29 +0300

    Lesson 4

---
+++ BasicAlgoritms/005-Swap(Method)/Program.cs
@@ -0,0 +1,27 @@
+﻿void Swap(ref int a,ref int b)//reference-ссылка
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+void Calculator(int a,int b, out int sum,out int multi)//out можно не присваивать значения
+{
+    sum=a+b;
+    multi=a*b;
+}
+
+int a=3,b=5;
+System.Console.WriteLine($"a={a} b={b}");
+Swap(ref a,ref b);
+System.Console.WriteLine($"a={a} b={b}");
+int sum,multi;//описываем переменные, в которые будут возвращаться значения
+Calculator(a,b,out sum,out multi);
+
+bool flag;
+int x;
+do
+{
+flag=int.TryParse(Console.ReadLine(),out x);// TryParse не выдаст исключительную ситуацию
+if (flag==false) System.Console.WriteLine("Введете целое число!");
+}
+while(flag==false);
---
+++ BasicAlgoritms/008-RandomArray(Массив из случайных чисел)/Program.cs
@@ -0,0 +1,53 @@
+﻿//Массив из случайных чисел
+
+/*
+int[] RandomIntArray(int size=10,int min=0,int max=100)
+{
+ int[] a=new int[size];
+ Random random=new Random();
+ for(int i=0;i<size;i++)
+ a[i]=random.Next(min,max);
+ return a;
+}
+*/
+
+void RandomIntArray(out int[] a, int size=10,int min = 0, int max= 100)
+{
+    a= new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max);    
+}
+
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+
+
+int[] m;
+RandomIntArray(out m,max:110,size:20);
+Print(m);
+
+/*
+int[] RandomIntArray(int size)
+{
+ int[] a=new int[size];//создание массива
+ Random random=new Random();//заполнение массива
+ for(int i=0;i<size;i++)
+ a[i]=random.Next(0,100);
+ return a;
+}
+
+void Print(int[] a)//вывод массива на экран
+{
+    for(int i=0;i<a.Length;i++)
+        System.Console.Write($"{a[i],5}");// отводим под массив 5 символов
+}
+
+int[] m=RandomIntArray(10);//ссылка на массив
+Print(m);
+*/
