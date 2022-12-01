f90e2fdae79c8b1032048c0b4cb6ff6085737526
Margarita <margoshka_d@mail.ru>
2022-12-01 23:13:58 +0300

    Homework 5

---
+++ 51/Program.cs
@@ -0,0 +1,35 @@
+﻿//51.С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+int[] RandomIntArray(int N, int min, int max)
+{
+    int[] a=new int[N];
+//заполнили массив
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+     a[i]=random.Next(-100,200);
+return a;     
+}
+
+void PrintArray(int[] a,int tab=5)
+{
+    string formatString="{0,"+tab+"}";
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write(formatString,a[i]);
+}
+
+void BubbleSort(int[]a,out int count)
+{
+    count=0;
+    for(int i=0;i<a.Length;i++)
+      
+        if(a[i]>0)
+        count++;
+}
+    
+
+int[] a=RandomIntArray(10,0,10);
+PrintArray(a);
+int count=0;
+BubbleSort(a,out count);
+System.Console.WriteLine();
+System.Console.WriteLine(count);
---
+++ 56/Program.cs
@@ -0,0 +1,31 @@
+﻿//56.Написать программу копирования массива
+
+int[] RandomIntArray(int N, int min, int max)
+{
+    int[] a=new int[N];
+
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+     a[i]=random.Next(0,200);
+return a;     
+}
+
+int[] array = new int[10];
+int arrayCopy;
+
+Console.Write("Оригинал ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 50);
+    Console.Write (array [i] + " ");
+}
+Console.WriteLine();
+array[2] = 17; 
+Console.Write(" Копия ");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy = array[i];
+    Console.Write(arrayCopy + " ");
+}
+
+
