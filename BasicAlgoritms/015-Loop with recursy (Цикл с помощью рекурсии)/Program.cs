ae91fd85109abb6e337b916b9c87d5e581a7a074
Margarita <margoshka_d@mail.ru>
2022-12-05 22:12:03 +0300

    Lesson 7

---
+++ BasicAlgoritms/015-Loop with recursy (Цикл с помощью рекурсии)/Program.cs
@@ -0,0 +1,20 @@
+﻿//Вывести числа от 1 до N без использования цикла
+//рекурсия-сами себя вызываем
+/*
+int i=1;
+int N=10;
+metka:
+    System.Console.WriteLine(i);
+    i++;
+    if (i<=N) goto metka;
+*/
+
+void Loop(int N,int i)
+{
+    if (i<=N)
+    {
+        System.Console.WriteLine(i);
+        Loop(N,i+1);// снова обращаемся к подпрограмме
+    }
+}
+Loop(10,1);
