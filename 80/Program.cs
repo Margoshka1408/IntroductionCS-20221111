27f203dd50a661a6b21a0ae35938f19899c24ce2
Margarita <margoshka_d@mail.ru>
2022-12-11 23:43:44 +0300

    Homework 8

---
+++ 80/Program.cs
@@ -0,0 +1,7 @@
+﻿//80.Вывести на экран кодировку символов по ASCII(с 32 до 122)
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+for (int i = a; i <= b; i++)
+    Console.WriteLine(i.ToString() + "-й символ таблицы ASCII: " + (char)i);
+System.Console.WriteLine();
---
+++ 82/Program.cs
@@ -0,0 +1,12 @@
+﻿//82.C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+
+string s=Console.ReadLine();
+int k=0;
+char tochka = '.';
+for(int i = 0; i < s.Length; i++) 
+{
+    if(!s[i].Equals(tochka)) k++;
+    else break;
+}
+System.Console.WriteLine(k);
+
