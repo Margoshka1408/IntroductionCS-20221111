af6e5de3181031ee0e2c6e4508745e1cca2aba56
Margarita <margoshka_d@mail.ru>
2022-12-12 23:22:26 +0300

    Lesson 9

---
+++ BasicAlgoritms/022-Delegates, Lambda, LINQ/Program.cs
@@ -0,0 +1,114 @@
+﻿//89. Подсчитать сумму цифр, встречающихся в строке
+
+//Делегаты
+/*
+int CountDigits(string s)//подсчитали количество цифр в строке
+ {
+   int count=0;
+    foreach(char c in s)
+    if (char.IsDigit(c))
+      count++;
+    return count;
+ }
+*/
+/*
+int CountLatin(string s)//подсчитали количество латинских букв в строке
+ {
+   int count=0;
+    foreach(char c in s)
+    if (c>='A' && c<='Z' || c>='a' && c<='z')//диапазон
+      count++;
+    return count;
+ }
+ */
+
+/*
+ int CountRus(string s)//подсчитали количество латинских букв в строке
+ {
+   int count=0;
+    foreach(char c in s)
+    if (c>='А' && c<='Я' || c>='а' && c<='я' || c=='Ё' || c=='ё')//диапазон букву ё рассматривают отдельно
+      count++;
+    return count;
+ }
+ */
+
+ delegate bool IsChar(char c);//сигнатура функции bool(char)
+ class Program
+{
+    /*
+     static int CountChars(string s, IsChar isChar)
+     {
+         int count=0;
+        foreach(char c in s)
+             if (isChar(c))
+                 count++;
+        return count;
+     }
+    */
+        static int CountChars(string s, Func<char,bool> isChar)//на вход char на выходе bool
+    {
+        int count=0;
+        foreach(char c in s)
+            if (isChar(c))
+                count++;
+        return count;
+    }
+
+    static bool IsLatin(char c)
+    {
+        return (c>='A' && c<='Z' || c>='a' && c<='z');
+    }
+
+    static  bool IsRus(char c)
+    {
+        return (c>='А' && c<='Я' || c>='а' && c<='я' || c=='Ё' || c=='ё');
+    }
+
+    static void Main()
+    {
+        string s="Эта строка содержит latinskie буквы и цифры(12345)";
+        System.Console.WriteLine(CountChars(s,IsLatin));
+        System.Console.WriteLine(CountChars(s,IsRus));
+    }
+}
+/*
+//Action click
+click=new Action(выделение)
+click=new Action(заливка)
+//Func готовый делегат
+*/
+
+
+/*
+        System.Console.WriteLine(CountChars(s,IsLatin));
+        System.Console.WriteLine(CountChars(s,delegate(char c)//анонимный делегат,
+        внутри вызова какого-то метода взять и описать какую-то функцию
+        {
+            return (c>='A' && c<='Z' || c>='a' && c<='z');
+        }
+        ));
+        
+        System.Console.WriteLine(CountChars(s,
+        (c)=>//лямбда-выражение многострочное
+        {
+            return (c>='A' && c<='Z' || c>='a' && c<='z');
+        }
+        ));
+        System.Console.WriteLine(CountChars(s,
+        (c)=>c>='A' && c<='Z' || c>='a' && c<='z'
+        //лямбда-выражение однострочное
+        ));
+        System.Console.WriteLine(CountChars(s,IsRus));
+        System.Console.WriteLine(CountChars(s,char.IsDigit));
+        //=> Лямбда выражения
+        */
+
+        /*
+        using System.Linq; язык в языке
+        string s="asd123";
+        var queary=(from c in s where (c>='A' && c<='Z' || c>='a' && c<='z') select c).Count();//запрос из строки.набор действий
+        var sum=(from c in s where char.IsDigit(c) select char.GetNumericValue(c)).Sum();
+        System.Console.WriteLine(sum);
+        string s1;
+        */
---
+++ BasicAlgoritms/023-Частотный словарь/Program.cs
@@ -0,0 +1,22 @@
+﻿//Частотный словарь
+//Составить частотный словарь элементов одномерного массива состоящих из чисел от 0 до 99.
+
+int[] data={1,3,4,8,3,5,8};
+int[] freqArr=new int[100];//частотный массив
+
+// foreach(int element in data)
+//     freqArr[element]++;//индексы элемента частотного массива
+// for(int i=0;i<freqArr.Length;i++)    
+//     if (freqArr[i]!=0)
+//       System.Console.WriteLine($"{i} {freqArr[i]}");
+
+Dictionary<int,int> freqDic=new Dictionary<int, int>();//словать ключ и значение
+
+foreach(int element in data)
+        if (freqDic.ContainsKey(element)) freqDic[element]++;//содержит ключ
+           else 
+             freqDic.Add(element,1);//добавляем новый элемент
+
+foreach(var el in freqDic) //пробегаемся по каждому элементу            
+    System.Console.WriteLine($"{el.Key} {el.Value}");
+     
